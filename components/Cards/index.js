b84dd87a2eaad7d8cd85249facc34354352f8553
Kadeem140 <Beckford.kadeem@yahoo.com>
2020-02-23 22:06:42 -0500

    Completed Cards Component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,96 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+    // const cardCon = document.querySelector('.cards-container');
+    // console.log(cardCon);
+
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+        .then(resolve => {
+                const cardCon = document.querySelector('.cards-container');
+                console.log(cardCon);
+            
+                console.log(resolve.data);
+                // const art = resolve.data.articles;
+
+                //  javascript, bootstrap, technology, jquery, node
+
+                const javascript = resolve.data.articles.javascript;
+                const bootstrap = resolve.data.articles.bootstrap;
+                const technology = resolve.data.articles.technology;
+                const jquery = resolve.data.articles.jquery;
+                const node = resolve.data.articles.node;
+                
+                javascript.forEach(e => {
+                    let newCard = Card(e)
+                    cardCon.appendChild(newCard); 
+
+                })
+
+                bootstrap.forEach(e => {
+                    let newCard = Card(e)
+                    cardCon.appendChild(newCard);
+                })
+
+                technology.forEach(e => {
+                    let newCard = Card(e)
+                    cardCon.appendChild(newCard);
+                })
+
+                jquery.forEach(e => {
+                    let newCard = Card(e)
+                    cardCon.appendChild(newCard);  
+                })
+
+                node.forEach(e => {
+                    let newCard = Card(e)
+                    cardCon.appendChild(newCard); 
+                })
+
+
+
+        })
+        .catch(err => console.log(err))
+
+
+    function Card(news) {
+        const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const author = document.createElement('div');
+        const imageContainer = document.createElement('div');
+        const image = document.createElement('img');
+        const name = document.createElement('span');
+
+        card.classList.add('card');
+        headline.classList.add('headline');
+        author.classList.add('author');
+        imageContainer.classList.add('img-container');
+
+        headline.innerHTML = news.headline;
+        image.src = news.authorPhoto;
+
+        name.innerHTML = `Written By: ${news.authorName}`;
+
+
+        //Appending elements
+        card.appendChild(headline);
+        card.appendChild(author);
+        author.appendChild(imageContainer);
+        author.appendChild(name);
+        imageContainer.appendChild(image);
+
+        return card;
+
+
+
+    }
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
+//
